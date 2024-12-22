bff2b859e34cb040ae9521b2d8b01b450852db03
Sagar-078 <sagarbehera7846@gmail.com>
2024-12-22 20:53:15 +0530

    checked

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
               source: "/api/:path*",
               headers: [
                   { key: "Access-Control-Allow-Credentials", value: "true" },
-                  { key: "Access-Control-Allow-Origin", value: "https://ecommerce-seller-pi.vercel.app/" }, 
+                  { key: "Access-Control-Allow-Origin", value: "https://ecommerce-seller-jx3611ff8-sagar-078s-projects.vercel.app/" }, 
                   { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                   { key: "Access-Control-Allow-Headers", value: "Content-Type,Authorization" },
                   
